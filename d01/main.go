ea20dde5667370f0065b2bf79e06f6c7047bd143
Guilherme Stoll Toaldo <gs.toaldo@gmail.com>
2024-12-02 10:06:34 -0300

    d01 p1

--- d01/main.go
+++ d01/main.go
@@ -1,7 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"slices"
+
+	"github.com/gstoaldo/advent-of-code-2024/utils"
+)
+
+func parse(filepath string) ([]int, []int) {
+	l1, l2 := []int{}, []int{}
+
+	re := regexp.MustCompile(`\d+`)
+
+	for _, line := range utils.ReadLines(filepath) {
+		matches := re.FindAllString(line, -1)
+
+		l1 = append(l1, utils.ToInt(matches[0]))
+		l2 = append(l2, utils.ToInt(matches[1]))
+	}
+
+	return l1, l2
+}
+
+func p1(l1, l2 []int) int {
+	slices.Sort(l1)
+	slices.Sort(l2)
+
+	sum := 0
+
+	for i := range l1 {
+		sum += utils.Abs(l1[i] - l2[i])
+	}
+
+	return sum
+}
 
 func main() {
-	fmt.Println("hello world")
+	l1, l2 := parse(utils.Filepath())
+
+	fmt.Println(p1(l1, l2))
 }
