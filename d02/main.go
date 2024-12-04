52dcd43d5abcba6165c79ae25a087d8f863478fb
Guilherme Stoll Toaldo <gs.toaldo@gmail.com>
2024-12-04 09:37:14 -0300

    d02 p2

--- d02/main.go
+++ d02/main.go
@@ -21,29 +21,67 @@ func parse(filepath string) (reports [][]int) {
 	return reports
 }
 
+func permutations(report []int) [][]int {
+	result := [][]int{}
+
+	for i := range report {
+		cp := append([]int{}, report...)
+		result = append(result, append(cp[:i], cp[i+1:]...))
+	}
+
+	return result
+}
+
+func isSafe(report []int) bool {
+	deltas := []int{}
+
+	for i := 1; i < len(report); i++ {
+		deltas = append(deltas, report[i]-report[i-1])
+	}
+
+	for _, v := range deltas {
+		if utils.Abs(v) > 3 || v == 0 {
+			return false
+		}
+	}
+
+	// check if it is always increasing or decreasing
+	for i := 1; i < len(deltas); i++ {
+		if deltas[i]*deltas[i-1] <= 0 {
+			return false
+		}
+	}
+
+	return true
+}
+
 func p1(reports [][]int) int {
 	result := 0
 
 	for _, report := range reports {
-		if report[1]-report[0] == 0 {
-			continue
+		if isSafe(report) {
+			result++
+		}
+	}
+
+	return result
+}
+
+func p2(reports [][]int) int {
+	result := 0
+
+	for _, report := range reports {
+		anyIsSafe := false
+
+		// Brute force: generate every possible permutation by removing one
+		// element from the slice and check if any permutation is safe.
+		// The size of the reports in the input is pretty small (max 8 elements),
+		// so it's not a big deal to use a brute force approach.
+		for _, permutation := range permutations(report) {
+			anyIsSafe = anyIsSafe || isSafe(permutation)
 		}
 
-		// sign = +1 for increasing, -1 for decreasing
-		sign := (report[1] - report[0]) / utils.Abs(report[1]-report[0])
-
-		safe := true
-
-		for i := 1; i < len(report); i++ {
-			value := (report[i] - report[i-1]) * sign
-
-			if value <= 0 || value > 3 {
-				safe = false
-				break
-			}
-		}
-
-		if safe {
+		if anyIsSafe {
 			result++
 		}
 	}
@@ -55,4 +93,5 @@ func main() {
 	reports := parse(utils.Filepath())
 
 	fmt.Println(p1(reports))
+	fmt.Println(p2(reports))
 }
