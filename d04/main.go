771fc4c7fdf5e289bfce8865c81891a13ec6539c
Guilherme Stoll Toaldo <gs.toaldo@gmail.com>
2024-12-06 10:43:17 -0300

    d04 p2

--- d04/main.go
+++ d04/main.go
@@ -48,7 +48,54 @@ func p1(lines []string) (result int) {
 	return result
 }
 
+func match(mask string, currMask string) bool {
+	for i, c := range mask {
+		if c == '*' {
+			continue
+		}
+
+		if string(currMask[i]) != string(c) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func p2(lines []string) (result int) {
+	masks := []string{
+		"M*M:*A*:S*S",
+		"M*S:*A*:M*S",
+		"S*M:*A*:S*M",
+		"S*S:*A*:M*M",
+	}
+
+	for i := 0; i < len(lines); i++ {
+		for j := 0; j < len(lines[0]); j++ {
+			maxI := i + 2
+			maxJ := j + 2
+
+			if !(maxI >= 0 && maxI < len(lines) && maxJ >= 0 && maxJ < len(lines[0])) {
+				// out of bounds
+				continue
+			}
+
+			currentMask := fmt.Sprintf("%s:%s:%s", lines[i][j:j+3], lines[i+1][j:j+3], lines[i+2][j:j+3])
+
+			for _, mask := range masks {
+				if match(mask, currentMask) {
+					result++
+					break
+				}
+			}
+		}
+	}
+
+	return result
+}
+
 func main() {
 	lines := utils.ReadLines(utils.Filepath())
 	fmt.Println(p1(lines))
+	fmt.Println(p2(lines))
 }
